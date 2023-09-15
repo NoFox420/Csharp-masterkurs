81a62a1ca25c17fb7d565ecd7e3b97013a2d219f
janfr <janfr@DESKTOP-7VHV2L0>
2023-09-15 20:47:00 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Csharp_masterkurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello("Jan", 30);
+        }
+        //Methoden werden in klassen definiert
+        //static methoden werden nicht auf objekten aufgerufen
+        //void definiert den rückgabewert
+
+        static void SayHello(string name, int age)
+        {
+            Console.WriteLine("Hallo {0}, du bist {1} Jahre alt!", name, age);
+        }
+    }
+}
