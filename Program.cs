35bc691156f9a60041189520f8ce6a6c13aae78a
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2023-09-16 20:59:58 +1000

    Übungsaufgabe 1, Modul 5, Fallunterscheidungen

--- Program.cs
+++ Program.cs
@@ -4,18 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Gebe einen Radius ein: ");
-            double radius = Convert.ToDouble(Console.ReadLine());
+            //Logische operatoren:
+            //&& UND , || ODER , ! NICHT
 
-            double result = GetCircleArea(radius);
-            Console.WriteLine("Die Fläche eines Kreises mit dem Radius {0}cm beträgt {1}cm²!", radius, result);
+            Console.WriteLine("Bitte gib dein Gewicht in KG an: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Bitte gib deine Größe in Metern an: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double calcBMI = BMI(weight, height);
+
+            if (calcBMI <= 18.4)
+            {
+                Console.WriteLine("Du hast Untergewicht!");
+            }
+            else if (calcBMI >=18.5 && calcBMI <= 24.9)
+            {
+                Console.WriteLine("Du hast Normalgewicht!");
+            }
+            else if (calcBMI >=25 &&  calcBMI <= 29.9)
+            {
+                Console.WriteLine("Du hast Übergewicht!");
+            }
+            else if (calcBMI >= 30 && calcBMI <= 34.9)
+            {
+                Console.WriteLine("Du hast Adipositas Grad 1 !");
+            }
+            else if (calcBMI >= 35 && calcBMI <= 39.9)
+            {
+                Console.WriteLine("Du hast Adipositas Grad 2!");
+            }
+            else if (calcBMI >= 40)
+            {
+                Console.WriteLine("Du hast Adipositas Grad 3!");
+            }
+
         }
 
-        static double GetCircleArea(double radius)
+       static int userBodyweight (string getWeight) 
         {
-
-            return Math.PI * radius*radius;
+            int weight = Convert.ToInt32(Console.ReadLine());
+            return weight;
         }
+        
+        static double userHeight (string getHeight) 
+        {
+            double height = Convert.ToDouble(Console.ReadLine());
+            return height;
+        
+        }
+
+        static double BMI (int weight, double height) 
+        {
+            double bmi = weight / (height * height);
+            Console.WriteLine("Dein BMI ist: {0}", bmi);
+            return bmi;
+            
+        }
+
         
     }
 }
