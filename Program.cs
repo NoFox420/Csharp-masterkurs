251c7f45ca2636e47e80b78930cfe297c0dea754
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2023-10-02 12:52:35 +1100

    Übungsaufgabe 3, Modul 7, Schleifen

--- Program.cs
+++ Program.cs
@@ -4,52 +4,22 @@
     {
         static void Main(string[] args)
         {
-            bool loop = true;
+            
 
-            while (loop)
-            {
+                Console.Write("Gib deine Zahl ein: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Gib deine erste Zahl ein: ");
-                double num1 = Convert.ToDouble(Console.ReadLine());
+                Calculation(num1);
 
-                Console.Write("Gib deinen Rechenoperator ein: ");
-                char operation = Convert.ToChar(Console.ReadLine());
-
-                Console.Write("Gib deine zweite Zahl ein: ");
-                double num2 = Convert.ToDouble(Console.ReadLine());
-
-                Console.Clear();
-
-                Calculation(num1, num2, operation);
-
-            }
+            
         }
 
-        static void Calculation (double num1, double num2, char operation)
+        static void Calculation (int num1)
         {
-            switch (operation)
+            for (int i = 1; i <= 10;  i++)
             {
-                case '+': double sum1 = num1 + num2;
-                    Console.WriteLine("{0} {1} {2} = {3}", num1, operation, num2, sum1);
-                    break;
-
-                case '-':
-                    double sum2 = num1 - num2;
-                    Console.WriteLine("{0} {1} {2} = {3}", num1, operation, num2, sum2);
-                    break;
-
-                case '*':
-                    double sum3 = num1 * num2;
-                    Console.WriteLine("{0} {1} {2} = {3}", num1, operation, num2, sum3);
-                    break;
-
-                case '/':
-                    double sum4 = num1 / num2;
-                    Console.WriteLine("{0} {1} {2} = {3}", num1, operation, num2, sum4);
-                    break;
-
-                default: Console.WriteLine("Ungültige Eingabe!");
-                    break;
+                int result = num1 * i;
+                Console.WriteLine("{0} * {1} = {2}", num1, i, result);
             }
         }
 
