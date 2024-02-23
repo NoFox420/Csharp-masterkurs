6752012ae40e827242ca92fe83314b5319201a29
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-02-23 21:01:43 +0900

    Typenkonvertierung Praxis, Modul 9

--- Program.cs
+++ Program.cs
@@ -4,28 +4,18 @@
     {
         static void Main(string[] args)
         {
-            //IMPLIZITE KONVERTIERUNG
-                //erfolgt automatisch, compiler weiß wann er konvertieren muss
-                //funktioniert von numerischem zu numerischem datentypen
-                //datenverlust muss ausgeschlossen sein
-
-            int zahl1 = 15;
-            double zahl2 = zahl1; //funktioniert
-
-            double zahl3 = 15;
-            float zahl4 = zahl3; //funktioniert nicht
+            //IMPLIZITE KONVERTIERUNG (AUTOMATISCH)
+            float float1 = 123.32F;
+            double double1 = float1;
 
 
-            //EXPLIZITE KONVERTIERUNG
-                //wenn implizit nicht möglich ist
-                //nur anwenden wenn datenverlust auszuschließen ist
-                //verwendung von cast-operator
+            //EXPLIZITE KONVERTIERUNG (MANUELL)
+            double double2 = 123.1;
+            float float2 = (float)double2;
 
-            double zahl5 = 3.1459;
-            float zahl6 = zahl5; //funktioniert nicht
-
-            double zahl7 = 3.1459;
-            float zahl8 = (float)zahl7; //funktioniert
+            //HILFSKLASSEN
+            float2 = Convert.ToSingle(double2);
+                
         }
 
     }
