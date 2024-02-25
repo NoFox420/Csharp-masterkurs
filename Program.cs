025676dd5c317d90f1aba28d590fe64aed35c68d
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-02-26 10:24:30 +1100

    Vererbung Praxis, Modul 10

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿using Csharp_masterkurs.Geometry;
-
+﻿
+using System;
 
 namespace Csharp_masterkurs
 
@@ -8,12 +8,53 @@ namespace Csharp_masterkurs
     {
         static void Main(string[] args)
         {
-            Rectangle rect = new Rectangle();
+            Hund coco = new Hund();
+            coco.Name = "Coco";
+            coco.Alter = 11;
+            coco.Rasse = "Weißer Schäferhund";
+            coco.Fressen();
+            coco.Bellen();
+
+            Wachhund jill = new Wachhund();
+            jill.Name = "Jill";
+            jill.Alter = 9;
+            jill.Rasse = "Mischling";
+            jill.BewacheHaus();
+            jill.Fressen();
+            jill.Bellen();
 
         }
 
     }
 
+    class Hund
+    {
+        //Eigenschaften
+        public string Name { get; set; }
+        public int Alter { get; set; }
+        public string Rasse { get; set; }
+
+        //Methoden
+        public void Fressen()
+        {
+            Console.WriteLine("Der Hund " + Name + " frisst gerade!");
+        }
+
+        public void Bellen()
+        {
+            Console.WriteLine("Der Hund " + Name + " bellt gerade!");
+        }
+    }
+
+    //Klasse erbt von andere Klasse
+    //um von mehreren klassen zu erben interfaces benutzen
+    class Wachhund : Hund
+    {
+        public void BewacheHaus()
+        {
+            Console.WriteLine("Der Hund " + Name + " bewacht das Haus!");
+        }
+    }
 
 
 }
