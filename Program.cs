239df474d8cc2339b4cb8b567804c96d0a9a9d36
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-06-08 13:33:35 +1000

    Übungsaufgabe 1, Modul 13, Polymorphie

--- Program.cs
+++ Program.cs
@@ -9,11 +9,28 @@ namespace Csharp_masterkurs
     {
         static void Main(string[] args)
         {
-            NewPerson jens = new NewPerson("Jens", "Bauer");
-            jens.Greet();
+            double[] numbers = new double[] {10, 5, 20, 30};
+            Console.WriteLine(Sum(numbers));
+            Console.WriteLine(Sum(10, 5));
+        }
 
-            NewPerson sabrina = new NewPerson("Sabrina", "Müller");
-            jens.Greet(sabrina);
+        static double Sum(double a, double b)
+        {
+            return a + b; 
+        }
+
+        static double Sum(double[] numbers) 
+        {
+            double sum = 0;
+
+            foreach (double number in numbers) 
+            {
+                sum += number;
+            }
+
+            return sum;
         }
     }
+
+    
 }
