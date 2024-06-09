5a73882be01488fd7157c7734e0b78ac841ec664
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-06-10 09:24:53 +1000

    Events definieren und auslösen, EventArgs verwenden, Modul 18

--- Program.cs
+++ Program.cs
@@ -11,45 +11,63 @@ namespace Csharp_masterkurs
     {
         static void Main(string[] args)
         {
-            //List erstellen und Methode einbinden
+            //6. List erstellen und Methode einbinden
             BetterList<string> names = new BetterList<string>();
+
+            //8. Added Event mit OnAdded Methode abonnieren
             names.Added += OnAdded;
 
             names.Add("Sabrina");
             names.Add("Alina");
             names.Add("Peter");
             names.Add("Uwe");
-
-
         }
 
-        static void OnAdded()
+        //7. Event Handler Methode
+        static void OnAdded(object sender, AddedEventArgs args)
         {
-            Console.WriteLine("Ein Objekt wurde zur List hinzugefügt...");
+            Console.WriteLine("Das Objekt: " + args.AddedItem.ToString() + " wurde hinzugefügt.");
         }
     }
 
-    //Delegaten instanziieren für Event
-    delegate void AddedEventHandler();
+    //3. Delegaten instanziieren für Event
+    delegate void AddedEventHandler(object sender, AddedEventArgs args);
 
-    //Generische List mit Platzhalter für Datentypen definieren
+    //1. Generische List mit Platzhalter für Datentypen definieren
     class BetterList<T> : List<T>
     {
-        //Add Methode der List Klasse wird erweitert mit einem Event
+        //2. Add Methode der List Klasse wird erweitert mit einem Event
         public new void Add(T item)
         {
             //Eigentliche Funktionalität der Methode wird hinzugefügt
             base.Add(item);
-
-            //Prüfen ob methoden dieses event abonniert haben
-            if (Added != null) 
-            {
-                //Event wird aufgerufen
-                Added();
-            }
+            //Event wird aufgerufen
+            OnAdded(item);
         }
 
-        //Event erstellen der delegate nutzt
+        //4. Event erstellen der delegate nutzt
         public event AddedEventHandler Added;
+
+        //5. Methode um Event zu starten
+        private void OnAdded(T item)
+        {
+            //Prüfen ob methoden dieses event abonniert haben
+            if (Added != null)
+            {
+                //Event wird aufgerufen
+                Added(this, new AddedEventArgs(item));
+            }
+        }
+    }
+
+    //Klasse erstellen die 
+    class AddedEventArgs : EventArgs 
+    {
+        public object AddedItem { get; set; }
+
+        public AddedEventArgs(object addedItem)
+        {
+            AddedItem = addedItem;
+        }
     }
 }
