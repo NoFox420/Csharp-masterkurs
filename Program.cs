9e299985ad27c394674e1b078d048b1d68fef28a
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-06-09 14:05:26 +1000

    Enumeratoren Praxis, Modul 14

--- Program.cs
+++ Program.cs
@@ -10,54 +10,27 @@ namespace Csharp_masterkurs
     {
         static void Main(string[] args)
         {
-            Point punkt = new Point(10, 5);
-            punkt.MovePosition(2, 2);
-            
-            Console.WriteLine(punkt.x + ", " + punkt.y);
+            Personen person1 = new Personen("Dirk Maier", Geschlecht.Männlich);
+            Console.WriteLine(person1.Geschlecht);
+        }     
+    }    
 
-            //Zeitdifferenz messen zwischen struct Point und class Point
-
-            Stopwatch sw = new Stopwatch();
-
-            Point[] punkte = new Point[10000000];
-
-            sw.Start();
-
-            for (int i = 0; i < punkte.Length; i++)
-            {
-                punkte[i] = new Point(i,i);
-            }
-
-            for (int i = 0;i < punkte.Length; i++)
-            {
-                punkte[i].MovePosition(1, 1);
-            }
-            sw.Stop();
-
-            Console.WriteLine("Fertig!");
-            Console.WriteLine("Verstrichene Zeit: " + sw.ElapsedMilliseconds);
-        }
-
-        
-    }
-
-    struct /*class*/ Point
+    enum Geschlecht
     {
-        public int x;
-        public int y;
-
-        public Point(int _x, int _y) 
-        {
-            x= _x;
-            y= _y;
-        }
-
-        public void MovePosition(int x_movement, int y_movement)
-        {
-            x += x_movement;
-            y += y_movement;
-        }
+        Männlich,
+        Weiblich,
+        Divers
     }
 
-    
+    class Personen
+    {
+        public string Name { get; set; }
+        public Geschlecht Geschlecht { get; set; }
+
+        public Personen (string name, Geschlecht geschlecht) 
+        {
+            Name = name;
+            Geschlecht = geschlecht;
+        }
+    }
 }
