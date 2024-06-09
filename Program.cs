921d11d30973eaa4d77f0c0e4ddf8c6908e1549b
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-06-09 15:52:13 +1000

    Übungsaufgabe 1, Modul 15, Collections

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Security.Cryptography.X509Certificates;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Csharp_masterkurs
 
@@ -10,27 +11,31 @@ namespace Csharp_masterkurs
     {
         static void Main(string[] args)
         {
-            Personen person1 = new Personen("Dirk Maier", Geschlecht.Männlich);
-            Console.WriteLine(person1.Geschlecht);
+            List<string> names = new List<string>();
+
+            names.Add("Peter");
+            names.Add("Alina");
+            names.Add("Sabine");
+            names.Add("Florian");
+
+            Console.WriteLine("Unsortiert!");
+            Console.WriteLine("--------------------");
+            foreach (string name in names) 
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine();
+
+            names.Sort();
+
+            Console.WriteLine("Sortiert");
+            Console.WriteLine("--------------------");
+            foreach (string name in names) 
+            {
+                Console.WriteLine(name);
+            }
+
         }     
-    }    
-
-    enum Geschlecht
-    {
-        Männlich,
-        Weiblich,
-        Divers
-    }
-
-    class Personen
-    {
-        public string Name { get; set; }
-        public Geschlecht Geschlecht { get; set; }
-
-        public Personen (string name, Geschlecht geschlecht) 
-        {
-            Name = name;
-            Geschlecht = geschlecht;
-        }
-    }
+    }      
 }
