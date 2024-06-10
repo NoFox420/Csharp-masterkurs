79d8e857a3fe4adea694b79d71e7ca9fd0f38880
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-06-11 07:57:09 +1000

    StreamWriter Praxis, Modul 22, Using-Statement

--- Program.cs
+++ Program.cs
@@ -14,14 +14,14 @@ namespace Csharp_masterkurs
         {
             //Objekt erstellen mit pfad und bool für append parameter
             string pfad = @"C:\Users\janfr\OneDrive\Desktop\TestOrdner\TestDatei.txt";
-            StreamWriter sw = new StreamWriter(pfad, true);
 
-            //öffnet einen Stream und schreibt in datei
-            sw.WriteLine("Hallo mein Name ist Jani");
-
-            //stream schließen
-            sw.Close();
-
+            //vereinfachte methode um in eine datei zu schreiben, schließt den stream automatisch
+            using (StreamWriter sw = new StreamWriter(pfad, true))
+            {
+                //öffnet einen Stream und schreibt in datei
+                sw.WriteLine("");
+                sw.WriteLine("Hallo mein Name ist Jani");
+            }
         }
     }
 }
